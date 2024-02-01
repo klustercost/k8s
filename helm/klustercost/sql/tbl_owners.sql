01e9421000ee6c2b684864383b0c55e942141c56
mihaielemnaru <v_mihai.lemnaru@jedox.com>
2024-02-01 12:02:59 +0200

    updated psql tables

---
+++ helm/klustercost/sql/tbl_owners.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA IF NOT EXISTS klustercost;
+CREATE TABLE klustercost.tbl_owners (
+    idx serial PRIMARY KEY,
+    name VARCHAR (100),
+    namespace VARCHAR (100),
+    record_time VARCHAR (100),
+    own_version VARCHAR (100),
+    own_kind VARCHAR (100),
+    own_uid VARCHAR (255),
+    owner_version VARCHAR (100),
+    owner_kind VARCHAR (100),
+    owner_uid VARCHAR (255),
+    labels VARCHAR (255)
+);
