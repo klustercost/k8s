946ea841ba1799c7262469c5da0b28c638e1c189
george-nutu <v_george.nutu@jedox.com>
2024-03-10 17:20:54 +0200

    Adjusted tbl_nodes structure

--- helm/klustercost/sql/tbl_nodes.sql
+++ helm/klustercost/sql/tbl_nodes.sql
@@ -1,11 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS klustercost;
 CREATE TABLE klustercost.tbl_nodes (
     idx serial PRIMARY KEY,
-    node_name VARCHAR (100),
-    node_insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    node_mem VARCHAR (100),
-    node_cpu VARCHAR (100),
-    node_uid VARCHAR (100),
-    labels VARCHAR (200),
+    node VARCHAR (100),
+    mem VARCHAR (100),
+    cpu VARCHAR (100),
     price_per_hour VARCHAR (100)
 );
