b82a11a7ca1e193c8de30702ae6e67da41b555ea
Radu Ialovoi <ialovoi@yalos-solutions.com>
2026-03-23 16:49:42 +0200

    cleanup code

--- wab/main.py
+++ wab/main.py
@@ -5,5 +5,4 @@ import os
 load_dotenv(override=False)
 
 if __name__ == "__main__":
-    test = os.getenv("HOST_PORT")
     uvicorn.run("src.hook:app", host="0.0.0.0", port=int(os.getenv("HOST_PORT")), reload=True)
--- wab/src/daemon.py
+++ wab/src/daemon.py
@@ -2,8 +2,6 @@ from . import logger
 from .send_message import send_message
 from .query_data import query
 
-
-
 def handle_message(message, phone_number_id):
     sender_id = message["from"]    
     response = query(message["text"]["body"])
