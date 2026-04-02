e5c9635f6b5876ad87b131cc598e6254af2e77ab
Radu Laurentiu Ialovoi <129099238+yalosvoios@users.noreply.github.com>
2026-04-02 16:41:01 +0300

    Refactor message handling to return only the natural message

--- wab/src/daemon.py
+++ wab/src/daemon.py
@@ -2,6 +2,7 @@ from . import logger
 from .send_message import send_templated_message
 from .query_data import query
 import os
+import json
 
 TEMPLATE = os.environ.get("TEMPLATE")
 
@@ -10,5 +11,6 @@ crt_context="_9dG8Nkit9?ctid=00a13d3a-b034-4fc8-b3b0-b1b37777fd93&pbi_source=lin
 def handle_message(message, phone_number_id):
     sender_id = message["from"]    
     response = query(message["text"]["body"])
-    logger.log.info("Handling from %s request %s ",sender_id,response)
-    send_templated_message(sender_id, TEMPLATE, phone_number_id, response, crt_context)
+    natural_response = json.loads(response)["natural"]
+    logger.log.info(f"Handling from ${sender_id} request ${response} with answer ${natural_response}")
+    send_templated_message(sender_id, TEMPLATE, phone_number_id, natural_response, crt_context)
