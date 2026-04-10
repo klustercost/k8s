bf1cfec7c1c3cef7f89e0745750fd554b4ea320a
Radu Ialovoi <ialovoi@yalos-solutions.com>
2026-04-10 19:03:52 +0300

    flexible configurator

--- chat/teams/config.py
+++ chat/teams/config.py
@@ -1,9 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 class Config:
-    CLIENT_ID = os.environ.get("CLIENT_ID", "")
-    BOT_TYPE = os.environ.get("BOT_TYPE", "")
-    MCP_CLIENT_ADDRESS = os.getenv("MCP_CLIENT_ADDRESS", "")
+    def __init__(self, *environment_variables):
+        load_dotenv()
+        for var in environment_variables:
+            setattr(self, var, os.environ.get(var, ""))
