8bbb4cd2ef313d17c4f30781915a3144d0efb606
Radu Ialovoi <ialovoi@yalos-solutions.com>
2026-04-10 13:01:38 +0300

    introduce teams bot as a container

---
+++ chat/teams/config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    """Bot Configuration"""
+
+    APP_ID = os.environ.get("CLIENT_ID", "")
+    APP_PASSWORD = os.environ.get("CLIENT_SECRET", "")
+    APP_TYPE = os.environ.get("BOT_TYPE", "")
+    APP_TENANTID = os.environ.get("TENANT_ID", "")
+    MCP_CLIENT_ADDRESS = os.getenv("MCP_CLIENT_ADDRESS", "")
