cdac2d6524dd80b41998031a1e8bfb1206739d5a
Radu Ialovoi <ialovoi@yalos-solutions.com>
2026-04-16 16:55:34 +0300

    query data for teams return onjects instead of strings

--- chat/teams/query_data.py
+++ chat/teams/query_data.py
@@ -4,6 +4,6 @@ import json
 def query(url: str, user_id: str, message: str) -> str:
     response = requests.post(f"{url}/ask", json={"user_id": user_id,"question": message})
     if response.status_code != 200:
-        return '{"internal error"}'
+        return {"error":"internal error"}
     else:
-        return json.loads(response.text)["answer"]
+        return json.loads(json.loads(response.text)["answer"])
