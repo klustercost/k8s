6b01641405a1b154582280d2e7dab2f9a38507d1
Radu Ialovoi <ialovoi@yalos-solutions.com>
2026-05-27 15:33:36 +0300

    cleanup code after the switch in persistence model

--- monitor/pkg/model/model.go
+++ monitor/pkg/model/model.go
@@ -1,11 +1,5 @@
 package model
 
-import (
-	"time"
-
-	promModel "github.com/prometheus/common/model"
-)
-
 type DataExchange map[string]interface{}
 
 // NodeMisc is a struct that contains the node miscellaneous information
@@ -20,79 +14,4 @@ type NodeMisc struct {
 	Region       string
 	Zone         string
 	OS           string
-}
-
-// This struct is used to store the memory and CPU usage of a pod
-// It is used to insert data into the database
-// Used by pod-controller.go
-type PodConsumption struct {
-	Memory *promModel.Sample
-	CPU    *promModel.Sample
-}
-
-type PodResources struct {
-	CPURequest *float64
-	CPULimit   *float64
-	MemRequest *float64
-	MemLimit   *float64
-}
-
-// This struct is used to store the owner_version, owner_kind, owner_name, owner_uid of a *v1.Pod
-// It is used to insert data into the database
-// Used by pod-controller.go and app-controller.go
-type OwnerReferences struct {
-	OwnerVersion string
-	OwnerKind    string
-	OwnerName    string
-	OwnerUid     string
-}
-
-// This struct is used to store the record_time, owner_uid, own_uid, labels node_name
-// It is used to insert data into the database
-// Used by pod-controller.go
-type PodMisc struct {
-	RecordTime time.Time
-	OwnerName  string
-	OwnUid     string
-	Labels     string
-	NodeName   string
-	AppLabel   string
-	Shard      int
-}
-
-// record_time, own_version, own_kind, own_uid, owner_version, owner_kind, owner_name, owner_uid, labels
-// Used by app-controller.go
-type AppOwnerReferences struct {
-	RecordTime   time.Time
-	OwnVersion   string
-	OwnKind      string
-	OwnUid       string
-	OwnerVersion string
-	OwnerKind    string
-	OwnerName    string
-	OwnerUid     string
-	Labels       string
-}
-
-// ServiceMisc is a struct that contains the node miscellaneous information
-// It is used to insert data into the database
-// Used by service-controller.go
-type ServiceMisc struct {
-	RecordTime time.Time
-	UID        string
-	AppLabel   string
-	Labels     string
-	Selector   string
-}
-
-// PodAppLabels contains the Kubernetes recommended app labels for a pod.
-// These map to the procedure parameters in klustercost.register_pod_data.
-// Used by pod-controller.go
-type PodAppLabels struct {
-	Name      string // app.kubernetes.io/name
-	Instance  string // app.kubernetes.io/instance
-	Version   string // app.kubernetes.io/version
-	Component string // app.kubernetes.io/component
-	PartOf    string // app.kubernetes.io/part-of
-	ManagedBy string // app.kubernetes.io/managed-by
 }
--- monitor/pkg/persistence/persistence_interface.go
+++ monitor/pkg/persistence/persistence_interface.go
@@ -4,8 +4,5 @@ import "klustercost/monitor/pkg/model"
 
 type Persistence interface {
 	InsertNode(string, *model.NodeMisc) error
-	InsertOwner(string, string, *model.AppOwnerReferences) error
-	InsertPod(string, string, string, string, *model.PodConsumption, *model.PodAppLabels, *model.PodResources) error
 	InsertPodJson(string) error
-	InsertService(string, string, *model.ServiceMisc) error
 }
